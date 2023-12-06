88f9de4a6512f8b9a539352f4dd94f037265cf1e
parkercornbrooks <pcorn123@gmail.com>
2023-12-06 18:48:17 -0500

    day6 part1

---
+++ day6/main.js
@@ -0,0 +1,66 @@
+const readFile = require('../utils/ReadFile')
+
+/*
+Basic calculations
+  timeAccelerated = raceTime - secHeld
+  distance = timeAccelerated * secHeld
+Therefore:
+  distance = total*secHeld - secHeld^2
+Translated to parabolic formula:
+  y = -1*x^2 + total*x + 0
+    a = -1
+    b = total
+    c = 0
+Translate down (-) by c (recordDistance) to find where the parabola crosses the y=c axis
+    c = -recordDistance
+*/
+
+const inputFile = 'input.txt'
+const numbersRegex = /\d+/g
+let times
+let distances
+
+function calculateTimeValues(raceTime, recordDistance) {
+  const a = -1
+  const b = raceTime
+  const c = -recordDistance
+
+  const x1 = (-b + Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a)
+  const x2 = (-b - Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a)
+  return [x1, x2]
+}
+
+function getWaysToWin(raceTime, recordDistance) {
+  const [min, max] = calculateTimeValues(raceTime, recordDistance)
+  const minisculeValue = 0.000000001
+  const minInt = Math.ceil(min+minisculeValue)
+  const maxInt = Math.floor(max-minisculeValue)
+  return maxInt-minInt + 1
+}
+
+function forLine(line) {
+  if (line.startsWith('Time')) {
+    const timeStrings = line.match(numbersRegex)
+    times = timeStrings.map(s => parseInt(s))
+  }
+  else if (line.startsWith('Distance')) {
+    const distanceStrings = line.match(numbersRegex)
+    distances = distanceStrings.map(s => parseInt(s))
+  }
+}
+
+function afterFile() {
+  let totalMultiplier = 1
+  times.forEach((time, index) => {
+    const distance = distances[index]
+    const waysToWin = getWaysToWin(time, distance)
+    totalMultiplier *= waysToWin
+  })
+  console.log('Part 1:', totalMultiplier)
+}
+
+readFile(
+  inputFile,
+  forLine,
+  afterFile,
+)
