1fcee58158800e2f35628bce9eed9a1b943cdf91
parkercornbrooks <pcorn123@gmail.com>
2023-12-08 01:16:05 -0500

    day8 part2 brute force failure

--- day8/main.js
+++ day8/main.js
@@ -3,8 +3,8 @@ const inputFile = 'input.txt'
 
 const instructionParseRegex = /[A-Z]{3}/g
 let firstLine = true
-const start = 'AAA'
-const end = 'ZZZ'
+const startNode = 'AAA'
+const endNode = 'ZZZ'
 
 let instructions
 const map = {}
@@ -24,18 +24,44 @@ function forLine(line) {
 }
 
 function afterFile() {
-  let current = start
+  traversePart1()
+  traversePart2()
+}
+
+function traversePart1() {
+  let currentNode = startNode
   let step = 0
-  while (current !== end) {
-    let index = step
-    if (index > instructions.length-1) {
-      index = step % instructions.length
-    }
+  while (currentNode !== endNode) {
+    let index = calculateIndex(step)
     const instruction = instructions[index]
-    current = map[current][instruction]
+    currentNode = map[currentNode][instruction]
     step++
   }
   console.log('Part 1:', step)
+}
+
+function traversePart2() {
+  let currentNodes = Object.keys(map).filter(k => k[2] === 'A')
+  let step = 0
+  while (!allEndWithZ(currentNodes)) {
+    let index = calculateIndex(step)
+    const instruction = instructions[index]
+    currentNodes = currentNodes.map(currentNode => map[currentNode][instruction])
+    step++
+  }
+  console.log('Part 2:', step)
+}
+
+function calculateIndex(step) {
+  let index = step
+  if (index > instructions.length-1) {
+    index = step % instructions.length
+  }
+  return index
+}
+
+function allEndWithZ(nodes) {
+  return nodes.every(item => item[2] === 'Z')
 }
 
 readFile(
