254067443201ac2bdc7b953647694a61df9cdfe1
parkercornbrooks <pcorn123@gmail.com>
2023-12-09 16:55:04 -0500

    day9 part2

--- day9/main.js
+++ day9/main.js
@@ -9,12 +9,12 @@ function forLine(line) {
   const sequence = line
     .match(/-?\d+/g)
     .map(n => parseInt(n))
-    .reverse()
+
+  const previousNumber = findPreviousNumberIn(sequence)
+  totalPart2 += previousNumber
+
   const nextNumber = findNextNumberIn(sequence)
   totalPart1 += nextNumber
-
-  const previousNumber = findNextNumberIn(sequence.reverse())
-  totalPart2 += previousNumber
 }
 
 function afterFile() {
@@ -22,29 +22,20 @@ function afterFile() {
   console.log('Part 2 Total:', totalPart2)
 }
 
-function findNextNumberIn(sequence) {
+function findPreviousNumberIn(sequence) {
   const sequences = [sequence]
-  for (let i=0; i<sequence.length-1; i++) {
-    for (let j = 0; j<i+1; j++) {
-      const backindex = i-j
-      const diff = sequences[j][backindex] - sequences[j][backindex+1]
-      if (backindex === 0) {
-        if (isPatternFound(sequences[j])) {
-          return sequences.reduce((a,c) => a+c[0], 0)
-        }
-        sequences[j+1] = [diff]
-      }
-      else {
-        sequences[j+1].push(diff)
-      }
+  for (let i=0; i<sequence.length-2; i++) {
+    sequences[i+1] = []
+    for (let j = 0; j<sequences[i].length-1; j++) {
+      const diff = sequences[i][j] - sequences[i][j+1]
+      sequences[i+1].push(diff)
     }
   }
   return sequences.reduce((a,c) => a+c[0], 0)
 }
 
-function isPatternFound(sequence) {
-  const isReady = sequence.every(num => num === 0)
-  return isReady
+function findNextNumberIn(sequence) {
+  return findPreviousNumberIn(sequence.reverse())
 }
 
 readFile(
